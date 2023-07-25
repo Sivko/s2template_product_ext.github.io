b3388ff41148ee9192ef0b477e8b8d8d270394eb
Vlad Sivakov <svv@salesap.ru>
2023-07-25 09:36:42 +0300

    add Styles

--- src/Render.js
+++ src/Render.js
@@ -62,8 +62,8 @@ export default function Render({ setRender, productsId, productsData, setProduct
     </div>
     {completed < 100 && <ProgressBar bgcolor={"#015692"} completed={completed} />}
     {completed >= 100 && (<>
-      <div id="errors">
-        {error.length && <div style={{ color: 'red', margin: '20px 0 10px' }}>Ошибки: </div>}
+      <div id="errors" style={{ marginBottom: '20px' }}>
+        {!!error.length && <div style={{ color: 'red', margin: '20px 0 10px' }}>Ошибки: </div>}
         {error.map(e => (<div key={Math.random()}>
           {/* {JSON.stringify(e)} */}
           <div>Тип: {e.type}</div>
@@ -74,195 +74,213 @@ export default function Render({ setRender, productsId, productsData, setProduct
       </div>
       <div id="document">
         {productsData.map(({ product, deal }) => (<div key={Math.random()}>
-          <table>
+          <table class="table_one">
             <tbody>
               <tr>
-                <td>Дата запуска</td>
-                <td>{moment(product?.attributes?.customs['custom-83033'])?.format('DD MMMM YYYY')}</td>
+                <td> <b>Дата запуска</b></td>
+                <td class="col"> <b>{moment(product?.attributes?.customs['custom-83033'])?.format('DD MMMM YYYY')} </b></td>
               </tr>
               <tr>
-                <td>Дата отгр. ПЛАН</td>
-                <td>{moment(product?.attributes?.customs['custom-83036'])?.format('DD MMMM YYYY')}</td>
+                <td><b>Дата отгр. ПЛАН</b></td>
+                <td class="col"><b>{moment(product?.attributes?.customs['custom-83036'])?.format('DD MMMM YYYY')}</b></td>
               </tr>
               <tr>
-                <td>Название</td>
-                <td>{product?.attributes?.name}</td>
+                <td><b>Название </b></td>
+                <td class="col2"><b>{product?.attributes?.name} </b></td>
               </tr>
               <tr>
                 <td>Описание</td>
-                <td>{product?.attributes?.description ? product?.attributes?.description.replace(/<\/?[^>]+(>|$)/g, "") : ''}</td>
+                <td class="col2">{product?.attributes?.description ? product?.attributes?.description.replace(/<\/?[^>]+(>|$)/g, "") : ''}</td>
               </tr>
               <tr>
                 <td>Кол-во</td>
-                <td>{product?.attributes?.quantity}</td>
+                <td class="col1">{product?.attributes?.quantity}</td>
+              </tr>
+            </tbody>
+          </table>
+          <br />
+          <table>
+            <tbody>
+              <tr>
+                <td class="none">Статус</td>
+                <td class="col2 none">{product?.attributes?.status}</td>
               </tr>
               <tr>
-                <td>Статус</td>
-                <td>{product?.attributes?.status}</td>
+                <td class="none">Заметка для производства</td>
+                <td class="col2 none">{product?.attributes?.customs['custom-117097']}</td>
               </tr>
               <tr>
-                <td>Заметка для производства</td>
-                <td>{product?.attributes?.customs['custom-117097']}</td>
+                <td class="none">Объект</td>
+                <td class="col2 none">{deal?.attributes?.name} №{deal?.attributes?.number}</td>
               </tr>
-              <tr>
-                <td>Объект</td>
-                <td>{deal?.attributes?.name} №{deal?.attributes?.number}</td>
-              </tr>
-              <tr>
-                <td><br /></td>
-                <td></td>
-              </tr>
+            </tbody>
+          </table>
+          <br />
+          <table class="table_two">
+            <tbody>
               <tr>
                 <td>К-1: &nbsp; основ-1 | BR-профиль</td>
-                <td>{product?.attributes?.customs['custom-108662']}</td>
+                <td class="col2">{product?.attributes?.customs['custom-108662']}</td>
               </tr>
               <tr>
                 <td>К-2: &nbsp; основ-2 | BR-загл</td>
-                <td>{product?.attributes?.customs['custom-108663']}</td>
+                <td class="col2">{product?.attributes?.customs['custom-108663']}</td>
               </tr>
               <tr>
                 <td>К-3: &nbsp; Фил SHTA | BR-панель</td>
-                <td>{product?.attributes?.customs['custom-108665']}</td>
+                <td class="col2">{product?.attributes?.customs['custom-108665']}</td>
               </tr>
               <tr>
                 <td>К-4: &nbsp; Трубки | BR-стекло</td>
-                <td>{product?.attributes?.customs['custom-111884']}</td>
+                <td class="col2">{product?.attributes?.customs['custom-111884']}</td>
               </tr>
               <tr>
                 <td>К-5: &nbsp; Разное; BR-стойка</td>
-                <td>{product?.attributes?.customs['custom-111885']}</td>
+                <td class="col2">{product?.attributes?.customs['custom-111885']}</td>
               </tr>
-              <tr>
-                <td><br /></td>
-                <td></td>
-              </tr>
+            </tbody>
+          </table>
+          <br />
+          <table class="table_three">
+            <tbody>
+
               <tr>
                 <td>СТ-01: &nbsp; Корпуса прямоугольные</td>
-                <td>{product?.attributes?.customs['custom-108664']}</td>
+                <td class="col2">{product?.attributes?.customs['custom-108664']}</td>
               </tr>
               <tr>
                 <td>СТ-02: &nbsp; Корпуса приборов</td>
-                <td>{product?.attributes?.customs['custom-112328']}</td>
+                <td class="col2">{product?.attributes?.customs['custom-112328']}</td>
               </tr>
               <tr>
                 <td>СТ-03: &nbsp; Аморт. BORE</td>
-                <td>{product?.attributes?.customs['custom-114316']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114316']}</td>
               </tr>
               <tr>
                 <td>СТ-04: &nbsp; BORE стойка</td>
-                <td>{product?.attributes?.customs['custom-114317']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114317']}</td>
               </tr>
               <tr>
                 <td>СТ-05: &nbsp; BR-Кронштейны</td>
-                <td>{product?.attributes?.customs['custom-114318']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114318']}</td>
               </tr>
               <tr>
                 <td>СТ-06: &nbsp; Переходники резьбы</td>
-                <td>{product?.attributes?.customs['custom-114319']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114319']}</td>
               </tr>
               <tr>
                 <td>СТ-07: &nbsp; Болт М6х10</td>
-                <td>{product?.attributes?.customs['custom-114320']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114320']}</td>
               </tr>
               <tr>
                 <td>СТ-08: &nbsp; Болт М6х16</td>
-                <td>{product?.attributes?.customs['custom-114321']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114321']}</td>
               </tr>
               <tr>
                 <td>СТ-09: &nbsp; Винты все</td>
-                <td>{product?.attributes?.customs['custom-114322']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114322']}</td>
               </tr>
               <tr>
                 <td>СТ-10: &nbsp; Гайки | Шайбы</td>
-                <td>{product?.attributes?.customs['custom-114323']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114323']}</td>
               </tr>
               <tr>
                 <td>СТ-11: &nbsp; Другое</td>
-                <td>{product?.attributes?.customs['custom-114324']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114324']}</td>
               </tr>
-              <tr>
-                <td><br /></td>
-                <td></td>
-              </tr>
+            </tbody>
+          </table>
+          <br />
+          <table class="table_four">
+            <tbody>
               <tr>
                 <td>ПУ-1: &nbsp; ГЛАВН</td>
-                <td>{product?.attributes?.customs['custom-114325']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114325']}</td>
               </tr>
               <tr>
                 <td>ПУ-2: &nbsp; SENS HT | ПУ SENS-18V | ПУ BOR</td>
-                <td>{product?.attributes?.customs['custom-114326']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114326']}</td>
               </tr>
               <tr>
                 <td>ПУ-3: &nbsp; SENS TC | ПУ SENS-30V | ПУ BOT</td>
-                <td>{product?.attributes?.customs['custom-114327']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114327']}</td>
               </tr>
               <tr>
                 <td>ПУ-4: &nbsp; П/П BOTE</td>
-                <td>{product?.attributes?.customs['custom-114328']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114328']}</td>
               </tr>
               <tr>
                 <td>ПУ-5: &nbsp; П/П BORE</td>
-                <td>{product?.attributes?.customs['custom-114329']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114329']}</td>
               </tr>
-              <tr>
-                <td><br /></td>
-                <td></td>
-              </tr>
+            </tbody>
+          </table>
+          <br />
+          <table class="table_five">
+            <tbody>
               <tr>
                 <td>ЭЛ-01: &nbsp; Сенсор</td>
-                <td>{product?.attributes?.customs['custom-114330']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114330']}</td>
               </tr>
               <tr>
                 <td>ЭЛ-02: &nbsp; Кабель 2-3-пров</td>
-                <td>{product?.attributes?.customs['custom-114331']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114331']}</td>
               </tr>
               <tr>
                 <td>ЭЛ-03: &nbsp; Кабель 4-5-пров</td>
-                <td>{product?.attributes?.customs['custom-114332']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114332']}</td>
               </tr>
               <tr>
                 <td>ЭЛ-04: &nbsp; Гермоввод М12 | Втулка кабеля</td>
-                <td>{product?.attributes?.customs['custom-114333']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114333']}</td>
               </tr>
               <tr>
                 <td>ЭЛ-05: &nbsp; Гермоввод М16</td>
-                <td>{product?.attributes?.customs['custom-114334']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114334']}</td>
               </tr>
               <tr>
                 <td>ЭЛ-06: &nbsp; Разъёмы</td>
-                <td>{product?.attributes?.customs['custom-114335']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114335']}</td>
               </tr>
               <tr>
                 <td>ЭЛ-07: &nbsp; Разъём ряд PLS</td>
-                <td>{product?.attributes?.customs['custom-114336']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114336']}</td>
               </tr>
               <tr>
                 <td>ЭЛ-08: &nbsp; Светодиод</td>
-                <td>{product?.attributes?.customs['custom-114337']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114337']}</td>
               </tr>
               <tr>
                 <td>ЭЛ-09: &nbsp; R-подстр-1 |Клеммы</td>
-                <td>{product?.attributes?.customs['custom-114338']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114338']}</td>
               </tr>
               <tr>
                 <td>ЭЛ-10: &nbsp; R-подстр-2 | Геркон СО2</td>
-                <td>{product?.attributes?.customs['custom-114339']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114339']}</td>
               </tr>
               <tr>
                 <td>ЭЛ-11: &nbsp; Диод | Реле</td>
-                <td>{product?.attributes?.customs['custom-114340']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114340']}</td>
               </tr>
               <tr>
                 <td>ЭЛ-12: &nbsp; Конденсатор CC</td>
-                <td>{product?.attributes?.customs['custom-114341']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114341']}</td>
               </tr>
               <tr>
                 <td>ЭЛ-13: &nbsp; Резистор RC</td>
-                <td>{product?.attributes?.customs['custom-114342']}</td>
+                <td class="col">{product?.attributes?.customs['custom-114342']}</td>
               </tr>
+            </tbody>
+          </table>
+
+
+          <br />
+          <table>
+            <tbody>
+
               <tr>
                 <td>ID</td>
-                <td>{product.id}</td>
+                <td class="col">{product.id}</td>
               </tr>
             </tbody>
           </table>
@@ -271,7 +289,7 @@ export default function Render({ setRender, productsId, productsData, setProduct
           <table className='tableFooter html2pdf__page-break'>
             <tbody>
               <tr>
-                <td>Название</td>
+                <td>{product?.attributes?.customs['custom-116884']}</td>
                 <td>Подпись
                   <br />
                 </td>
@@ -284,7 +302,20 @@ export default function Render({ setRender, productsId, productsData, setProduct
               </tr>
               <tr>
                 <td>
+                  {product?.attributes?.customs['custom-116877']}
+                </td>
+                <td>
                   <br />
+                </td>
+                <td>
+                </td>
+                <td>
+                  <br />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  {product?.attributes?.customs['custom-116878']}
                 </td>
                 <td>
                   <br />
@@ -298,8 +329,7 @@ export default function Render({ setRender, productsId, productsData, setProduct
               </tr>
               <tr>
                 <td>
-                  <br />
-                </td>
+                  {product?.attributes?.customs['custom-116879']}                </td>
                 <td>
                   <br />
                 </td>
@@ -312,8 +342,7 @@ export default function Render({ setRender, productsId, productsData, setProduct
               </tr>
               <tr>
                 <td>
-                  <br />
-                </td>
+                  {product?.attributes?.customs['custom-116880']}                </td>
                 <td>
                   <br />
                 </td>
@@ -326,8 +355,7 @@ export default function Render({ setRender, productsId, productsData, setProduct
               </tr>
               <tr>
                 <td>
-                  <br />
-                </td>
+                  {product?.attributes?.customs['custom-116881']}                </td>
                 <td>
                   <br />
                 </td>
@@ -340,8 +368,7 @@ export default function Render({ setRender, productsId, productsData, setProduct
               </tr>
               <tr>
                 <td>
-                  <br />
-                </td>
+                  {product?.attributes?.customs['custom-116882']}                </td>
                 <td>
                   <br />
                 </td>
@@ -354,22 +381,7 @@ export default function Render({ setRender, productsId, productsData, setProduct
               </tr>
               <tr>
                 <td>
-                  <br />
-                </td>
-                <td>
-                  <br />
-                </td>
-                <td>
-                  <br />
-                </td>
-                <td>
-                  <br />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <br />
-                </td>
+                  {product?.attributes?.customs['custom-116883']}                </td>
                 <td>
                   <br />
                 </td>
